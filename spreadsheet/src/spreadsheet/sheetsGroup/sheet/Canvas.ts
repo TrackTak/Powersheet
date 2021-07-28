aef4db27b52325a95c75f0cbc61a73c45940b155
Martin Dawson <MDawson@marketaxess.com>
2021-07-28 21:05:41 +0100

    added grid-lines

--- spreadsheet/src/spreadsheet/sheetsGroup/sheet/Canvas.ts
+++ spreadsheet/src/spreadsheet/sheetsGroup/sheet/Canvas.ts
@@ -1,25 +1,30 @@
 import { Layer } from 'konva/lib/Layer';
-import { NodeConfig } from 'konva/lib/Node';
-import { Stage } from 'konva/lib/Stage';
+import { Line, LineConfig } from 'konva/lib/shapes/Line';
+import { Stage, StageConfig } from 'konva/lib/Stage';
+import Col from './Col';
+import Row from './Row';
 
-interface ICreateParams {
-  width?: NodeConfig['width'];
-  height?: NodeConfig['height'];
-  [index: string]: any;
+interface ICreateStageConfig extends Omit<StageConfig, 'container'> {
+  container?: HTMLDivElement;
 }
 
-interface IConstructor extends ICreateParams {}
+interface IConstructor {
+  stageConfig?: ICreateStageConfig;
+  rows: Row[];
+  cols: Col[];
+}
 
 class Canvas {
   public container!: HTMLDivElement;
   public stage!: Stage;
   public layer!: Layer;
 
-  constructor(params: IConstructor = {}) {
-    this.create(params);
+  constructor(params: IConstructor) {
+    this.create(params.stageConfig);
+    this.drawGridLines(params.rows, params.cols);
   }
 
-  create({ width, height, ...stageConfig }: ICreateParams = {}) {
+  create(stageConfig: ICreateStageConfig = {}) {
     const id = 'powersheet-canvas';
 
     this.container = document.createElement('div');
@@ -40,6 +45,42 @@ class Canvas {
     this.stage.add(this.layer);
 
     this.layer.draw();
+  }
+
+  drawGridLines(rows: Row[], cols: Col[]) {
+    const lineConfig: LineConfig = {
+      stroke: '#c6c6c6',
+    };
+
+    const getHorizontalGridLine = (y: number) => {
+      return new Line({
+        ...lineConfig,
+        points: [0, 0, this.stage.width(), 0],
+        strokeWidth: 0.6,
+        y,
+      });
+    };
+
+    const getVerticalGridLine = (x: number) => {
+      return new Line({
+        ...lineConfig,
+        points: [0, 0, 0, this.stage.height()],
+        strokeWidth: 0.6,
+        x,
+      });
+    };
+
+    rows.forEach((row) => {
+      const horizontalGridLine = getHorizontalGridLine(row.number * row.height);
+
+      this.layer.add(horizontalGridLine);
+    });
+
+    cols.forEach((col) => {
+      const verticalGridLine = getVerticalGridLine(col.number * col.width);
+
+      this.layer.add(verticalGridLine);
+    });
   }
 }
 

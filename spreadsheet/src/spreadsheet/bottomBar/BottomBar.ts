1104f96150a6dffe9132fc5ad0b51a1e4da897a8
krisolchova <kristina.olchova@yahoo.com>
2021-08-11 18:44:15 +0100

    addd item vents

--- spreadsheet/src/spreadsheet/bottomBar/BottomBar.ts
+++ spreadsheet/src/spreadsheet/bottomBar/BottomBar.ts
@@ -2,56 +2,116 @@ import './bottomBar.css';
 import plusIcon from '../../icons/plus-icon.svg';
 import sheetIcon from '../../icons/sheet-icon.svg';
 
-export interface IBottomBar {}
+export interface IBottomBar extends IConstructorParams {}
+
+interface IConstructorParams {
+  addSheetButton: HTMLButtonElement;
+  menuSheetButton: HTMLButtonElement;
+  tabSheetButton: HTMLButtonElement;
+  addItem: HTMLUListElement;
+}
+
+const addItem = (button: HTMLButtonElement) => {
+  const list = document.createElement('ul');
+  const listItem = document.createElement('li');
+
+  list.appendChild(listItem);
+  listItem.appendChild(button);
+
+  return list;
+};
+
+const createAddSheetButton = () => {
+  const buttonPlus = document.createElement('button');
+
+  buttonPlus.addEventListener('click', () => {
+    console.log('click1');
+  });
+
+  const plusImage = document.createElement('img');
+
+  plusImage.src = plusIcon;
+  plusImage.alt = 'plus';
+  plusImage.className = 'plus-icon';
+  buttonPlus.appendChild(plusImage);
+
+  return buttonPlus;
+};
+
+const createMenuSheetButton = () => {
+  const buttonMenuSheet = document.createElement('button');
+
+  const menuSheetImage = document.createElement('img');
+
+  menuSheetImage.src = sheetIcon;
+  menuSheetImage.alt = 'sheet';
+  menuSheetImage.className = 'sheet-icon';
+
+  buttonMenuSheet.appendChild(menuSheetImage);
+
+  return buttonMenuSheet;
+};
+
+const createTabButton = (sheetNumber: number) => {
+  const buttonTab = document.createElement('button');
+  buttonTab.textContent = `Sheet${sheetNumber}`;
+
+  buttonTab.addEventListener('click', () => {
+    console.log('sheet1');
+  });
+
+  return buttonTab;
+};
 
 class BottomBar implements IBottomBar {
   element!: HTMLDivElement;
-  button!: HTMLButtonElement;
+  addSheetButton: HTMLButtonElement;
+  menuSheetButton: HTMLButtonElement;
+  tabSheetButton: HTMLButtonElement;
+  addItem: HTMLUListElement;
+  buttonTabs: HTMLButtonElement[];
 
-  constructor() {
+  constructor(params?: IConstructorParams) {
+    this.addSheetButton = params?.addSheetButton ?? createAddSheetButton();
+    this.menuSheetButton = params?.menuSheetButton ?? createMenuSheetButton();
+    this.tabSheetButton = createTabButton();
+    this.addItem = addItem(this.addSheetButton);
+
     this.create();
   }
 
   private create() {
     this.element = document.createElement('div');
-
     this.element.className = 'navbar';
 
-    // const container = document.createElement('div');
-    // this.element.className = 'wrapper';
+    const buttonWrapper = document.createElement('div');
+    buttonWrapper.className = 'wrapper';
 
-    // this.element.appendChild(container);
+    this.element.appendChild(buttonWrapper);
 
-    const buttonPlus = document.createElement('button');
+    const tabContainer = document.createElement('div');
+    tabContainer.className = 'tab';
 
-    buttonPlus.addEventListener('click', () => {
-      console.log('click1');
+    this.element.appendChild(tabContainer);
+
+    addItem(this.addSheetButton);
+
+    buttonWrapper.appendChild(this.addSheetButton);
+    buttonWrapper.appendChild(this.menuSheetButton);
+    tabContainer.appendChild(this.tabSheetButton);
+
+    this.addSheetButton.addEventListener('click', () => {
+      const tabSheetButton = createTabButton(this.buttonTabs.length);
+
+      this.buttonTabs.push(tabSheetButton);
+
+      //Empty tab container children
+      tabContainer.innerHTML = '';
+
+      this.buttonTabs.forEach((buttonTab) => {
+        tabContainer.appendChild(buttonTab);
+      });
     });
-
-    this.element.appendChild(buttonPlus);
-
-    const buttonSheet = document.createElement('button');
-
-    buttonSheet.addEventListener('click', () => {
-      console.log('click2');
-    });
-
-    this.element.appendChild(buttonPlus);
-    this.element.appendChild(buttonSheet);
-
-    const plusImage = document.createElement('img');
-    const sheetImage = document.createElement('img');
-
-    plusImage.src = plusIcon;
-    plusImage.alt = 'plus';
-    plusImage.className = 'plus-icon';
-
-    sheetImage.src = sheetIcon;
-    sheetImage.alt = 'sheet';
-    sheetImage.className = 'sheet-icon';
-
-    buttonPlus.appendChild(plusImage);
-    buttonSheet.appendChild(sheetImage);
   }
 }
 

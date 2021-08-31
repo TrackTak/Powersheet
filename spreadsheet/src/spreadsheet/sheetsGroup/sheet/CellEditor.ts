42304e17d4c8e20df26bc095428d9ed5f2b2c79b
Martin Dawson <MDawson@marketaxess.com>
2021-08-31 21:35:55 +0100

    renname canvas -> sheet

--- spreadsheet/src/spreadsheet/sheetsGroup/sheet/CellEditor.ts
+++ spreadsheet/src/spreadsheet/sheetsGroup/sheet/CellEditor.ts
@@ -1,6 +1,6 @@
 import { IRect } from 'konva/lib/types';
 import events from '../../events';
-import Canvas from './Canvas';
+import Sheet from './Sheet';
 import styles from './CellEditor.module.scss';
 
 import { DelegateInstance, delegate } from 'tippy.js';
@@ -11,8 +11,8 @@ class CellEditor {
   private cellTooltip: DelegateInstance;
   private isEditing = false;
 
-  constructor(private canvas: Canvas) {
-    this.canvas = canvas;
+  constructor(private sheet: Sheet) {
+    this.sheet = sheet;
 
     this.textArea = document.createElement('div');
     this.textArea.setAttribute('contentEditable', 'true');
@@ -24,21 +24,21 @@ class CellEditor {
       theme: 'cell',
       offset: [0, 5],
     });
-    this.canvas.container.appendChild(this.textArea);
+    this.sheet.container.appendChild(this.textArea);
 
     window.addEventListener('keydown', this.keyHandler);
-    this.canvas.shapes.sheet.on('dblclick', this.showCellEditor);
-    this.canvas.shapes.sheet.on('click', this.hideCellEditor);
-    this.canvas.stage.on('click', this.hideCellEditor);
-    this.canvas.stage.on('mousedown', this.hideCellEditor);
-    this.canvas.eventEmitter.on(events.scroll.horizontal, this.handleScroll);
-    this.canvas.eventEmitter.on(events.scroll.vertical, this.handleScroll);
+    this.sheet.shapes.sheet.on('dblclick', this.showCellEditor);
+    this.sheet.shapes.sheet.on('click', this.hideCellEditor);
+    this.sheet.stage.on('click', this.hideCellEditor);
+    this.sheet.stage.on('mousedown', this.hideCellEditor);
+    this.sheet.eventEmitter.on(events.scroll.horizontal, this.handleScroll);
+    this.sheet.eventEmitter.on(events.scroll.vertical, this.handleScroll);
   }
 
   destroy() {
     window.removeEventListener('keydown', this.keyHandler);
-    this.canvas.eventEmitter.off(events.resize.col.start, this.hideCellEditor);
-    this.canvas.eventEmitter.off(events.resize.row.start, this.hideCellEditor);
+    this.sheet.eventEmitter.off(events.resize.col.start, this.hideCellEditor);
+    this.sheet.eventEmitter.off(events.resize.row.start, this.hideCellEditor);
     this.cellTooltip.destroy();
   }
 
@@ -57,7 +57,7 @@ class CellEditor {
   };
 
   private showCellEditor = () => {
-    const selectedCell = this.canvas.selector.getSelectedCell();
+    const selectedCell = this.sheet.selector.getSelectedCell();
     const cellRect = selectedCell.children![0] as Rect;
     const absolutePosition = selectedCell.getAbsolutePosition();
     const cellPosition = {
@@ -91,11 +91,11 @@ class CellEditor {
 
   private showCellTooltip = () => {
     if (this.isEditing) {
-      const selectedCell = this.canvas.selector.getSelectedCell();
+      const selectedCell = this.sheet.selector.getSelectedCell();
       const row = selectedCell.attrs.row;
       const col = selectedCell.attrs.col;
-      const rowText = this.canvas.row.getHeaderText(row.x);
-      const colText = this.canvas.col.getHeaderText(col.x);
+      const rowText = this.sheet.row.getHeaderText(row.x);
+      const colText = this.sheet.col.getHeaderText(col.x);
 
       this.cellTooltip.setContent(`${colText}${rowText}`);
       this.cellTooltip.show();
@@ -103,8 +103,8 @@ class CellEditor {
   };
 
   private handleScroll = () => {
-    const rowScrollOffset = this.canvas.row.scrollBar.scrollOffset;
-    const colScrollOffset = this.canvas.col.scrollBar.scrollOffset;
+    const rowScrollOffset = this.sheet.row.scrollBar.scrollOffset;
+    const colScrollOffset = this.sheet.col.scrollBar.scrollOffset;
 
     if (rowScrollOffset.index || colScrollOffset.index) {
       this.showCellTooltip();

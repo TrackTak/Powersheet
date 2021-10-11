f3bb7339b2e83d12bbfd57ead0d8c24fdd330d32
Martin Dawson <u1356770@gmail.com>
2021-10-11 16:41:20 +0100

    update landing age for new features list

--- packages/themes/gatsby-theme-flexiblocks/src/homepage/saas-v2/index.jsx
+++ packages/themes/gatsby-theme-flexiblocks/src/homepage/saas-v2/index.jsx
@@ -7,13 +7,9 @@ import Divider from '@solid-ui-components/Divider'
 import ModalWithTabs from '@solid-ui-blocks/Modal/Block01'
 import ModalSimple from '@solid-ui-blocks/Modal/Block02'
 import Header from '@solid-ui-blocks/Header/Block01'
-import Tabs from '@solid-ui-components/Tabs'
 import Hero from '@solid-ui-blocks/Hero/Block01'
-import Features from '@solid-ui-blocks/Features/Block06'
-import FeatureOne from '@solid-ui-blocks/FeaturesWithPhoto/Block02'
-import FeatureTwo from '@solid-ui-blocks/FeaturesWithPhoto/Block05'
-import FeatureThree from '@solid-ui-blocks/FeaturesWithPhoto/Block01'
-import Screenshot from '@solid-ui-blocks/FeaturesWithPhoto/Block03'
+import FeaturesOne from '@solid-ui-blocks/Features/Block06'
+import FeaturesTwo from '@solid-ui-blocks/Features/Block07'
 import Process from '@solid-ui-blocks/Features/Block03'
 import Testimonials from '@solid-ui-blocks/Testimonials/Block03'
 import Companies from '@solid-ui-blocks/Companies/Block01'
@@ -45,52 +41,10 @@ const IndexPage = props => {
       </Container>
       <Divider space='4' />
       <Divider space='4' />
-      <Features content={content['features-one']} />
-      <Container variant='wide' sx={styles.tabsContainer}>
-        <Tabs space={5}>
-          <FeatureOne content={content['tab-feature-one']} />
-          <FeatureTwo content={content['tab-feature-two']} />
-          <FeatureThree content={content['tab-feature-three']} />
-        </Tabs>
-      </Container>
-      <Divider space='5' />
-      <Divider space='5' />
-      <Divider space='5' />
-      <Container variant='wide' sx={styles.tabsContainer}>
-        <Tabs space={3} variant='dots' position='bottom' arrows>
-          <Screenshot content={content['screenshot-one']} />
-          <Screenshot content={content['screenshot-two']} />
-          <Screenshot content={content['screenshot-three']} />
-        </Tabs>
-      </Container>
-      <Divider space='5' />
-      <Divider space='5' />
-      <Divider space='5' />
-      <Process content={content['process']} />
-      <Divider space='5' />
-      <Divider space='5' />
-      <Container variant='full' sx={styles.socialProofContainer}>
-        <Divider space='5' />
-        <Divider space='5' />
-        <Container variant='narrow'>
-          <Stats content={content['stats']} />
-          <Divider space='5' />
-        </Container>
-        <Testimonials content={content['testimonials']} />
-        <Divider space='5' />
-      </Container>
-      <Divider space='5' />
-      <Divider space='5' />
-      <Pricing content={content['pricing']} />
-      <Divider space='5' />
-      <Companies content={content['companies']} />
-      <Divider space='5' />
-      <Container variant='narrow'>
-        <Faq content={content['faq']} />
-      </Container>
-      <Divider space='5' />
-      <Divider space='5' />
-      <Blog content={content['latest-blogs']} />
+      <FeaturesOne content={content['features-one']} />
+      <Divider space='4' />
+      <Divider space='4' />
+      <FeaturesTwo content={content['features-two']} />
       <Divider space='5' />
       <Footer content={content['footer']} />
     </Layout>

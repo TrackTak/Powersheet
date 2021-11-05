0f7b3ff767a5b0b30b39b0d9dd1a0e260791767c
krisolchova <kristina.olchova@yahoo.com>
2021-11-05 21:23:43 +0000

    mock data and scss variables added

--- packages/spreadsheet/src/stories/Powersheet.stories.ts
+++ packages/spreadsheet/src/stories/Powersheet.stories.ts
@@ -24,17 +24,7 @@ import { AlwaysSparse, ConfigParams, HyperFormula } from 'hyperformula';
 import { getTTFinancialPlugin, finTranslations } from './getTTFinancialPlugin';
 import realExampleDataJSON from './mocks/realExampleData.json';
 import mockFinancialDataJSON from './mocks/mockFinancialData.json';
-import FunctionHelper from '../spreadsheet/functionHelper/FunctionHelper';
 import { ICustomRegisteredPluginDefinition } from '../spreadsheet/Exporter';
-//@ts-ignore
-import {
-  incomeStatementAttributes,
-  balanceSheetAttributes,
-  cashFlowStatementAttributes,
-  riskPremiumsAndBetasAttributes,
-  generalAttributes,
-  otherAttributes,
-} from './mocks/financialStatements';
 
 const realExampleData = realExampleDataJSON as ISpreadsheetData;
 
@@ -133,73 +123,6 @@ const buildSpreadsheetWithEverything = (
   const formulaBar = new FormulaBar();
   const exporter = new Exporter(customRegisteredPluginDefinitions);
   const bottomBar = new BottomBar();
-  const functionHelper = new FunctionHelper({
-    header: 'FINANCIAL - FIN',
-    headerDescription:
-      'Fetches current or historical securities information from Tracktak Finance.',
-    codeSyntaxUsage: [
-      '=FIN("revenue")',
-      '=FIN("revenue",,"01/01/2000")',
-      '=FIN("B2, B3")',
-    ],
-    codeSyntaxElements: [
-      {
-        codeSyntax: '=FIN(attribute, [startDate], [endDate])',
-        values: [
-          {
-            syntaxName: '[]',
-            description:
-              'indicates optional. If a parameter is optional it can be skipped with a comma. Example: =FIN("revenue",,"01/01/2021")',
-          },
-          {
-            syntaxName: 'attribute',
-            description:
-              '1st argument is required. Fetches current or historical securities information from Tracktak.',
-          },
-          {
-            syntaxName: '[startDate]',
-            description:
-              '2nd argument is optional. The start date when fetching historical data.',
-          },
-          {
-            syntaxName: '[endDate]',
-            description:
-              '3rd argument is optional. The end date when fetching historical data.',
-          },
-          {
-            syntaxName: '[mm/dd/yyy]',
-            description: 'The start date and end date format.',
-          },
-        ],
-      },
-    ],
-    attributes: [
-      {
-        header: 'Income Statement',
-        attributeNames: incomeStatementAttributes,
-      },
-      {
-        header: 'Balance Sheet',
-        attributeNames: balanceSheetAttributes,
-      },
-      {
-        header: 'Cashflow Statement',
-        attributeNames: cashFlowStatementAttributes,
-      },
-      {
-        header: 'Risk Premiums and Betas',
-        attributeNames: riskPremiumsAndBetasAttributes,
-      },
-      {
-        header: 'General',
-        attributeNames: generalAttributes,
-      },
-      {
-        header: 'Other',
-        attributeNames: otherAttributes,
-      },
-    ],
-  });
 
   const trueArgs: [string, string] = ['TRUE', '=TRUE()'];
   const falseArgs: [string, string] = ['FALSE', '=FALSE()'];
@@ -218,13 +141,11 @@ const buildSpreadsheetWithEverything = (
     formulaBar,
     exporter,
     bottomBar,
-    functionHelper,
   });
 
   spreadsheet.spreadsheetEl.prepend(formulaBar.formulaBarEl);
   spreadsheet.spreadsheetEl.prepend(toolbar.toolbarEl);
   spreadsheet.spreadsheetEl.appendChild(bottomBar.bottomBarEl);
-  spreadsheet.sheets.sheetEl.appendChild(functionHelper.functionHelperEl);
 
   return spreadsheet;
 };
